3efbe1f52a83fbc7d3cc0b4977a3c5169ca545a3
abed-binary <abed@deriv.com>
2021-03-19 11:23:46 +0800

    Revert plugin changes to package.

--- packages/analytics/example/android/app/src/main/kotlin/com/deriv/analytics_example/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.deriv.analytics_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- packages/analytics/ios/Classes/AnalyticsPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface AnalyticsPlugin : NSObject<FlutterPlugin>
-@end
