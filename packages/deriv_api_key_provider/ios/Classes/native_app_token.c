ca47f672018cd03aabe65dae99eccf5e44514f5f
hamed-fs <hamed@firstsource.io>
2021-09-20 12:21:01 +0430

    hamed/minor_refactor
    - minor refactor

--- packages/deriv_api_key_provider/ios/Classes/native_app_token.c
+++ packages/deriv_api_key_provider/ios/Classes/native_app_token.c
@@ -11,11 +11,6 @@ char* gdat() {
     return DEVELOPMENT_APP_TOKEN;
 }
 
-char* gpat() {
-    int length = strlen(PRODUCTION_APP_TOKEN_SEED);
-    char* result = (char*) malloc(sizeof(char) * length);
-    
-    strcpy(result, PRODUCTION_APP_TOKEN_SEED);
-    
-    return result;
+char* gpat() {    
+    return PRODUCTION_APP_TOKEN_SEED;
 }
