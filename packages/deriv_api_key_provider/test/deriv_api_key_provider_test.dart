f7e43578a1ff4dd8b59dea780f1f059491d855ff
hamed-fs <hamed@firstsource.io>
2021-09-20 12:22:18 +0430

    hamed/minor_refactor
    - minor refactor

--- packages/deriv_api_key_provider/test/deriv_api_key_provider_test.dart
+++ packages/deriv_api_key_provider/test/deriv_api_key_provider_test.dart
@@ -5,11 +5,11 @@ import 'package:deriv_api_key_provider/native_app_token.dart';
 void main() {
   test(
     'test development api token getter.',
-    () => expect(developmentAppToken, 'deriv_api_development_token'),
+    () => expect(developmentAppToken, 'DERIV_API_TOKEN'),
   );
 
   test(
     'test production api token getter.',
-    () => expect(productionAppToken, 'deriv_api_production_seed'),
+    () => expect(productionAppToken, 'PRODUCTION_DERIV_API_TOKEN_SEED'),
   );
 }
