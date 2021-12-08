c8b3d7d03738d2403cd888fef43aab3389a39733
Farzin Mirzaie <farzin@firstsource.tech>
2021-12-08 12:46:19 +0330

    Extract colors into new calss

--- packages/deriv_theme/lib/theme_provider.dart
+++ packages/deriv_theme/lib/theme_provider.dart
@@ -1,8 +1,9 @@
 // ignore_for_file: public_member_api_docs
 
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide Colors;
 import 'package:flutter/scheduler.dart';
 
+import 'colors/colors.dart';
 import 'src/colors.dart';
 import 'src/dimens.dart';
 import 'text_styles.dart';
@@ -64,54 +65,73 @@ class ThemeProvider {
   String get fontFamily => TextStyles.appFontFamily;
 
   // Colors getters
+  @Deprecated('Use `colors` instead')
   Color get brandCoralColor => BrandColors.coral;
 
+  @Deprecated('Use `colors` instead')
   Color get brandGreenishColor => BrandColors.greenish;
 
+  @Deprecated('Use `colors` instead')
   Color get brandOrangeColor => BrandColors.orange;
 
+  @Deprecated('Use `colors` instead')
   Color get accentRedColor =>
       _isDarkTheme ? DarkThemeColors.accentRed : LightThemeColors.accentRed;
 
+  @Deprecated('Use `colors` instead')
   Color get accentGreenColor =>
       _isDarkTheme ? DarkThemeColors.accentGreen : LightThemeColors.accentGreen;
 
+  @Deprecated('Use `colors` instead')
   Color get accentYellowColor => _isDarkTheme
       ? DarkThemeColors.accentYellow
       : LightThemeColors.accentYellow;
 
+  @Deprecated('Use `colors` instead')
   Color get base01Color =>
       _isDarkTheme ? DarkThemeColors.base01 : LightThemeColors.base01;
 
+  @Deprecated('Use `colors` instead')
   Color get base02Color =>
       _isDarkTheme ? DarkThemeColors.base02 : LightThemeColors.base02;
 
+  @Deprecated('Use `colors` instead')
   Color get base03Color =>
       _isDarkTheme ? DarkThemeColors.base03 : LightThemeColors.base03;
 
+  @Deprecated('Use `colors` instead')
   Color get base04Color =>
       _isDarkTheme ? DarkThemeColors.base04 : LightThemeColors.base04;
 
+  @Deprecated('Use `colors` instead')
   Color get base05Color =>
       _isDarkTheme ? DarkThemeColors.base05 : LightThemeColors.base05;
 
+  @Deprecated('Use `colors` instead')
   Color get base06Color =>
       _isDarkTheme ? DarkThemeColors.base06 : LightThemeColors.base06;
 
+  @Deprecated('Use `colors` instead')
   Color get base07Color =>
       _isDarkTheme ? DarkThemeColors.base07 : LightThemeColors.base07;
 
+  @Deprecated('Use `colors` instead')
   Color get base08Color =>
       _isDarkTheme ? DarkThemeColors.base08 : LightThemeColors.base08;
 
+  @Deprecated('Use `colors` instead')
   Color get greenColor =>
       _isDarkTheme ? DarkThemeColors.green : LightThemeColors.green;
 
+  @Deprecated('Use `colors` instead')
   Color get informationColor =>
       _isDarkTheme ? DarkThemeColors.information : LightThemeColors.information;
 
+  @Deprecated('Use `colors` instead')
   Color get disabledColor =>
       _isDarkTheme ? DarkThemeColors.disabled : LightThemeColors.disabled;
+
+  Colors get colors => Colors(isDark: _isDarkTheme);
 
   @visibleForTesting
   TextStyle getStyle({
