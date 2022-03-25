a657577bacccaed5b2cee7f22c9dff95a1ae1b84
sneha-solutelabs <sneha.s@solutelabs.com>
2022-03-25 17:42:16 +0530

    add notification count for live chat

--- packages/deriv_live_chat/android/src/main/kotlin/com/deriv/app/deriv_live_chat/DerivLiveChatPlugin.kt
+++ packages/deriv_live_chat/android/src/main/kotlin/com/deriv/app/deriv_live_chat/DerivLiveChatPlugin.kt
@@ -1,8 +1,10 @@
 package com.deriv.app.deriv_live_chat
+import MethodCallType
 import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.util.Log
+import android.view.View
 import androidx.annotation.NonNull
 import com.livechatinc.inappchat.ChatWindowConfiguration
 import com.livechatinc.inappchat.ChatWindowErrorType
@@ -17,14 +19,15 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import java.lang.invoke.MethodType
 import java.util.*
 
 /** DerivLiveChatPlugin */
 class DerivLiveChatPlugin: FlutterPlugin, MethodCallHandler , ActivityAware, EventChannel.StreamHandler{
   private lateinit var channel: MethodChannel
   var activity: Activity? = null
+
   var lifecycleSink: EventSink? = null
+  var chatWindowView : ChatWindowView ?= null
 
   companion object {
     const val CHANNEL = "derivLiveChat"
@@ -47,54 +50,56 @@ class DerivLiveChatPlugin: FlutterPlugin, MethodCallHandler , ActivityAware, Eve
       val groupId = call.argument<String>("groupId")
       val visitorName = call.argument<String>("visitorName")
       val visitorEmail = call.argument<String>("visitorEmail")
-      val chatWindowView = ChatWindowView.createAndAttachChatWindowInstance(activity!!)
+      if(chatWindowView == null ){
+        chatWindowView = ChatWindowView.createAndAttachChatWindowInstance(activity!!)
+        val configuration = ChatWindowConfiguration.Builder()
+          .setLicenceNumber(licenseNo)
+          .setGroupId(groupId)
+          .setVisitorName(visitorName)
+          .setVisitorEmail(visitorEmail)
+          .setCustomParams(customParams)
+          .build()
 
-      val configuration = ChatWindowConfiguration.Builder()
-        .setLicenceNumber(licenseNo)
-        .setGroupId(groupId)
-        .setVisitorName(visitorName)
-        .setVisitorEmail(visitorEmail)
-        .setCustomParams(customParams)
-        .build()
-
-      chatWindowView.setUpWindow(configuration)
-      chatWindowView.setUpListener(object : ChatWindowView.ChatWindowEventsListener {
-        override fun onChatWindowVisibilityChanged(visible: Boolean) {
-          if(visible){
-            lifecycleSink?.success("chatOpen")
-          }else{
-            lifecycleSink?.success("chatClose")
-          }
-        }
-
-        override fun onNewMessage(message: NewMessageModel?, windowVisible: Boolean) {
-          lifecycleSink?.success(message?.text)
-        }
-
-        override fun onStartFilePickerActivity(intent: Intent?, requestCode: Int) {
-
-        }
-
-        override fun onError(
-          errorType: ChatWindowErrorType?,
-          errorCode: Int,
-          errorDescription: String?
-        ): Boolean {
-          return true
-        }
-
-        override fun handleUri(uri: Uri?): Boolean {
-          return true
-        }
-      })
-
-      chatWindowView.initialize()
-      chatWindowView.showChatWindow()
+        chatWindowView?.setUpWindow(configuration)
+        chatWindowView?.setUpListener(chatListener)
+        chatWindowView?.initialize()
+      }
+      chatWindowView?.showChatWindow()
       result.success(null)
     } else {
       result.notImplemented()
     }
 
+  private val chatListener = object : ChatWindowView.ChatWindowEventsListener {
+    override fun onChatWindowVisibilityChanged(visible: Boolean) {
+      if(visible){
+        lifecycleSink?.success("chatOpen")
+      }else{
+        lifecycleSink?.success("chatClose")
+      }
+    }
+
+    override fun onNewMessage(message: NewMessageModel?, windowVisible: Boolean) {
+      Log.d("chatListner", message?.text.toString())
+      lifecycleSink?.success(message?.text)
+    }
+
+    override fun onStartFilePickerActivity(intent: Intent?, requestCode: Int) {
+
+    }
+
+    override fun onError(
+      errorType: ChatWindowErrorType?,
+      errorCode: Int,
+      errorDescription: String?
+    ): Boolean {
+      return true
+    }
+
+    override fun handleUri(uri: Uri?): Boolean {
+      return true
+    }
+  }
   override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
     channel.setMethodCallHandler(null)
   }
@@ -104,7 +109,7 @@ class DerivLiveChatPlugin: FlutterPlugin, MethodCallHandler , ActivityAware, Eve
   }
 
   override fun onDetachedFromActivityForConfigChanges() {
-
+    lifecycleSink = null
   }
 
   override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
