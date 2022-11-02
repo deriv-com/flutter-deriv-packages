77fc94674946de0b111a4005065ecc41e0599e48
Mohammad Teimouri <teimouri.mohammad@firstsource.tech>
2022-11-02 16:12:41 +0330

    Fix export

--- packages/deriv_auth/lib/deriv_auth.dart
+++ packages/deriv_auth/lib/deriv_auth.dart
@@ -1,4 +1,5 @@
 export 'src/auth/auth_cubit.dart';
+export 'src/auth/auth_error.dart';
 export 'src/login_cubit/login_cubit.dart';
 export 'src/models/account/account.dart';
 export 'src/auth/models/authorize.dart';
