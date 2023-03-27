f61ee4a2b87e32d546752fcf246845921027ebcd
hamed-deriv <57184669+hamed-deriv@users.noreply.github.com>
2023-03-27 14:58:54 +0800

    hamed/minor_refactor

--- packages/deriv_dependency_injector/test/type_factory_test.dart
+++ packages/deriv_dependency_injector/test/type_factory_test.dart
@@ -4,51 +4,56 @@ import 'package:deriv_dependency_injector/dependency_injector.dart';
 
 void main() {
   group('type factory tests =>', () {
-    late TypeFactory<MyClass> typeFactory;
+    late TypeFactory<SampleClass> typeFactory;
     late Injector injector;
 
     setUp(() {
       injector = Injector();
 
-      typeFactory = TypeFactory<MyClass>(
+      typeFactory = TypeFactory<SampleClass>(
         factoryFunction:
-            (Injector injector, Map<String, dynamic>? parameters) => MyClass(),
+            (Injector injector, Map<String, dynamic>? parameters) =>
+                SampleClass(),
         isSingleton: true,
       );
     });
 
     test('get() returns a new instance if not a singleton.', () {
-      final TypeFactory<MyClass> typeFactoryNotSingleton = TypeFactory<MyClass>(
+      final TypeFactory<SampleClass> typeFactoryNotSingleton =
+          TypeFactory<SampleClass>(
         factoryFunction:
-            (Injector injector, Map<String, dynamic>? parameters) => MyClass(),
+            (Injector injector, Map<String, dynamic>? parameters) =>
+                SampleClass(),
         isSingleton: false,
       );
-      final MyClass? instance1 = typeFactoryNotSingleton.get(injector, null);
-      final MyClass? instance2 = typeFactoryNotSingleton.get(injector, null);
+      final SampleClass? instance1 =
+          typeFactoryNotSingleton.get(injector, null);
+      final SampleClass? instance2 =
+          typeFactoryNotSingleton.get(injector, null);
 
       expect(instance1, isNot(same(instance2)));
     });
 
     test('get() returns the same instance if singleton.', () {
-      final MyClass? instance1 = typeFactory.get(injector, null);
-      final MyClass? instance2 = typeFactory.get(injector, null);
+      final SampleClass? instance1 = typeFactory.get(injector, null);
+      final SampleClass? instance2 = typeFactory.get(injector, null);
 
       expect(instance1, same(instance2));
     });
 
     test('get() passes additionalParameters to factory function.', () {
-      final TypeFactory<MyClass> typeFactoryWithParameters =
-          TypeFactory<MyClass>(
+      final TypeFactory<SampleClass> typeFactoryWithParameters =
+          TypeFactory<SampleClass>(
         factoryFunction:
             (Injector injector, Map<String, dynamic>? parameters) =>
-                MyClass.withParams(parameters!),
+                SampleClass.withParams(parameters!),
         isSingleton: false,
       );
       final Map<String, String> additionalParameters = <String, String>{
         'foo': 'bar',
       };
 
-      final MyClass? instance =
+      final SampleClass? instance =
           typeFactoryWithParameters.get(injector, additionalParameters);
 
       expect(instance?.foo, equals('bar'));
@@ -56,11 +61,11 @@ void main() {
   });
 }
 
-class MyClass {
-  MyClass({this.foo});
+class SampleClass {
+  SampleClass({this.foo});
 
-  factory MyClass.withParams(Map<String, dynamic> parameters) =>
-      MyClass(foo: parameters['foo']);
+  factory SampleClass.withParams(Map<String, dynamic> parameters) =>
+      SampleClass(foo: parameters['foo']);
 
   final String? foo;
 }
