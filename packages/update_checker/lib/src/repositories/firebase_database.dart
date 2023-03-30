b3379d59bfaa1199b2ab39ab340f7c490c7ff09e
ramin-deriv <ramin.vakili@deriv.com>
2023-03-30 18:39:10 +0800

    make update checker field as a parameter

--- packages/update_checker/lib/src/repositories/firebase_database.dart
+++ packages/update_checker/lib/src/repositories/firebase_database.dart
@@ -6,13 +6,16 @@ import 'package:firebase_database/firebase_database.dart';
 /// the firebase database.
 class FirebaseDatabaseRepository {
   /// Initializes the Firebase Database repository
-  const FirebaseDatabaseRepository();
+  const FirebaseDatabaseRepository({this.updateCheckerFirebaseField = 'build'});
+
+  /// The filed which update checker should pick from the Firebase database.
+  final String updateCheckerFirebaseField;
 
   /// Fetches the update information from the database.
   Future<dynamic> fetchUpdateData() async {
     final DatabaseReference dbRef = FirebaseDatabase.instance
         .reference()
-        .child('build')
+        .child(updateCheckerFirebaseField)
         .child(Platform.operatingSystem);
     return (await dbRef.once()).value;
   }
