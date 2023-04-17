2965a4de8adaec5f34b76ac2cb4113d48667efee
hamed-deriv <57184669+hamed-deriv@users.noreply.github.com>
2023-04-17 11:10:39 +0800

    hamed/update_env_tests

--- packages/deriv_env/test/env_test.dart
+++ packages/deriv_env/test/env_test.dart
@@ -18,7 +18,7 @@ void main() {
 
       expect(Env().isInitialized, isTrue);
 
-      expect(Env().entries.length, 6);
+      expect(Env().entries.length, 7);
 
       expect(Env().entries['STRING_VAR'], 'hello world');
       expect(Env().entries['INT_VAR'], '123');
@@ -26,6 +26,10 @@ void main() {
       expect(Env().entries['BOOL_VAR'], 'true');
       expect(Env().entries['VAR_WITH_EQUALS'], 'hello=world');
       expect(Env().entries['VAR_WITH_HASH'], 'hello#world');
+      expect(
+        Env().entries['ENCRYPTED_VAR'],
+        'ksBYGk15pXBqJ6t1eHLBP1K3IY/I2Hpb4jyAPYHB+Lc=:iWtwcoF4inlm0pFlg4N5l2U=',
+      );
     });
 
     test('get() method should return default value if key is not found.',
@@ -56,6 +60,20 @@ void main() {
       expect(Env().get<bool>('BOOL_VAR'), isTrue);
     });
 
+    test(
+        'get() method should parse value with a parser factory if it is provided.',
+        () async {
+      await Env().load('test/.env.test');
+
+      expect(
+        Env().get<double>(
+          'DOUBLE_VAR',
+          paserFactory: (String value) => double.parse(value) * 2,
+        ),
+        6.28,
+      );
+    });
+
     test('check handling variables with special characters like `#` and `=`.',
         () async {
       await Env().load('test/.env.test');
@@ -64,8 +82,30 @@ void main() {
       expect(Env().entries['VAR_WITH_HASH'], 'hello#world');
     });
 
+    test('handle encrypted variable.', () async {
+      await Env().load('test/.env.test');
+
+      expect(
+        Env().get<String>(
+          'ENCRYPTED_VAR',
+          encrypted: true,
+          decryptionKey: 'SECRET_KEY',
+        ),
+        'ecnrypted message',
+      );
+    });
+
     test('throws an exception if file is empty.', () async {
       expect(() => Env().load('test/.env.empty.test'), throwsException);
+    });
+
+    test(
+        'throws an exception if encrypted is true but no encryptionKey is proveided.',
+        () async {
+      expect(
+        () => Env().get<String>('valid_key', encrypted: true),
+        throwsException,
+      );
     });
 
     test('throws an exception if env is not initialized.', () async {
