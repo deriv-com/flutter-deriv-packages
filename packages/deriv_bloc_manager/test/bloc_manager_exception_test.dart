baafa298c1d256ce3067183348465ef2ca82b817
hamed-deriv <57184669+hamed-deriv@users.noreply.github.com>
2023-05-30 11:34:48 +0800

    hamed/add_bloc_manager_exception_tests

--- packages/deriv_bloc_manager/test/bloc_manager_exception_test.dart
+++ packages/deriv_bloc_manager/test/bloc_manager_exception_test.dart
@@ -3,12 +3,26 @@ import 'package:test/test.dart';
 import 'package:flutter_deriv_bloc_manager/manager.dart';
 
 void main() {
-  group('bloc manager exception test =>', () {
-    test('toString returns correct string.', () {
+  group('BlocManagerException tests =>', () {
+    test('should have the correct message when toString() is called.', () {
       final BlocManagerException exception =
-          BlocManagerException(message: 'test message');
+          BlocManagerException(message: 'Test message');
 
-      expect('$exception', '$BlocManagerException: test message');
+      expect('$exception', equals('BlocManagerException: Test message'));
+    });
+
+    test('should throw an exception with the provided message.', () {
+      final BlocManagerException exception =
+          BlocManagerException(message: 'Test message');
+
+      expect(exception.message, equals('Test message'));
+    });
+
+    test('should throw a runtime exception.', () {
+      final BlocManagerException exception =
+          BlocManagerException(message: 'Test message');
+
+      expect(exception, isA<Exception>());
     });
   });
 }
