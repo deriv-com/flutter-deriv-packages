ad7fc8aa3ffe4d349c26204d910d6dd0f8496b4f
Bassam El Obeid <bassam.elobeid@regentmarkets.com>
2023-06-07 14:08:25 +0400

    added `deriv_ui` widgets and utils

---
+++ packages/deriv_ui/lib/utils/animation_durations.dart
@@ -0,0 +1,5 @@
+/// Duration used for changing colors in animated widgets
+const Duration colorChangeDuration = Duration(milliseconds: 300);
+
+/// Duration used for changing size in animated widgets
+const Duration sizeChangeDuration = Duration(milliseconds: 300);
