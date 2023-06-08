04ff7f7d828cd9ecd2b02c6f2b4d5ae429c6c8cd
sahani-deriv <sahani@deriv.com>
2023-06-08 13:48:14 +0800

    wip: login layout test

--- packages/deriv_auth_ui/test/features/deriv_login_layout_test.dart
+++ packages/deriv_auth_ui/test/features/deriv_login_layout_test.dart
@@ -1,12 +1,13 @@
 import 'package:deriv_auth/deriv_auth.dart';
 import 'package:deriv_auth_ui/deriv_auth_ui.dart';
+import 'package:deriv_auth_ui/src/features/login/widgets/deriv_social_auth_panel.dart';
+import 'package:deriv_ui/deriv_ui.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:patrol/patrol.dart';
 
+import '../mocks.dart';
 import '../pump_app.dart';
-
-class MockAuthCubit extends Mock implements DerivAuthCubit {}
 
 void main() {
   group('DerivLoginLayout', () {
@@ -15,26 +16,52 @@ void main() {
     setUpAll(() {
       authCubit = MockAuthCubit();
     });
-    patrolTest('onLoggedIn is called when login is successful', ($) async {
-      mockOnLoggedIn(_) {}
+    patrolTest('renders email and password field including social auth buttons',
+        ($) async {
+      final mockAuthState = DerivAuthLoggedOutState();
 
-      final mockLoggedInState = DerivAuthLoggedInState(const AuthorizeEntity());
-
-      when(() => authCubit.state).thenAnswer((_) => mockLoggedInState);
+      when(() => authCubit.state).thenAnswer((_) => mockAuthState);
 
       when(() => authCubit.stream)
-          .thenAnswer((_) => Stream.fromIterable([mockLoggedInState]));
+          .thenAnswer((_) => Stream.fromIterable([mockAuthState]));
 
-      $.pumpApp(DerivLoginLayout(
+      await $.pumpApp(DerivLoginLayout(
         authCubit: authCubit,
         onResetPassTapped: () {},
         onSignupTapped: () {},
-        onLoginError: (p0) {},
-        onLoggedIn: mockOnLoggedIn,
+        onLoginError: (_) {},
+        onLoggedIn: (_) {},
         onSocialAuthButtonPressed: (p0) {},
       ));
 
-      verify(() => mockOnLoggedIn).called(1);
+      expect($(DerivLoginLayout), findsOneWidget);
+      expect($(BaseTextField).$('Email'), findsOneWidget);
+      expect($(BaseTextField).$('Password'), findsOneWidget);
+      expect($(DerivSocialAuthPanel), findsOneWidget);
+    });
+
+    patrolTest('calls onLoggedIn on successful login', ($) async {
+      final mockAuthState = DerivAuthLoggedInState(const AuthorizeEntity());
+
+      when(() => authCubit.state).thenAnswer((_) => mockAuthState);
+
+      when(() => authCubit.stream)
+          .thenAnswer((_) => Stream.fromIterable([mockAuthState]));
+
+      var onLoggedInCalled = false;
+
+      await $.pumpApp(DerivLoginLayout(
+        authCubit: authCubit,
+        onResetPassTapped: () {},
+        onSignupTapped: () {},
+        onLoginError: (_) {},
+        onLoggedIn: (_) {
+          onLoggedInCalled = true;
+        },
+        onSocialAuthButtonPressed: (p0) {},
+      ));
+
+      expect(onLoggedInCalled, isTrue);
     });
   });
 }
