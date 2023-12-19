d6e041abb304d806c45d36d16c5446585035d13c
horam-deriv <64075768+horam-deriv@users.noreply.github.com>
2023-12-19 14:12:21 +0800

    remove key and url logs

--- packages/deriv_feature_flag/lib/growthbook/growth_book_config.dart
+++ packages/deriv_feature_flag/lib/growthbook/growth_book_config.dart
@@ -5,18 +5,10 @@ import 'package:growthbook_sdk_flutter/growthbook_sdk_flutter.dart';
 /// A class that contains the whole GrowthBook config data.
 class GrowthBookConfig {
   /// Determine the GrowthBook client api key.
-  static String get gbClientKey {
-    final key = Env().get<String>('GROWTHBOOK_API_KEY');
-    logger.log('GrowthBook key is $key');
-    return key;
-  }
+  static String get gbClientKey => Env().get<String>('GROWTHBOOK_API_KEY');
 
   /// Determine the Growth Book Host URL.
-  static String get gbHostUrl {
-    final url = Env().get<String>('GROWTHBOOK_HOST_URL');
-    logger.log('GrowthBook url is $url');
-    return url;
-  }
+  static String get gbHostUrl => Env().get<String>('GROWTHBOOK_HOST_URL');
 
   /// Convert client side FeatureFlags to GrowthBook FeatureFlags.
   static Map<String, GBFeature> gbFeatures(Map<String, dynamic> features) {
