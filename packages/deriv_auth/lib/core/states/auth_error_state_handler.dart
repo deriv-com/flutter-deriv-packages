c20510176fe91de3f23a505a16b4906bc1faa689
sahani-deriv <sahani@deriv.com>
2024-02-08 11:05:04 +0800

    revert(deriv_auth): error state handler changes

--- packages/deriv_auth/lib/core/states/auth_error_state_handler.dart
+++ packages/deriv_auth/lib/core/states/auth_error_state_handler.dart
@@ -5,8 +5,7 @@ import 'package:flutter/material.dart';
 /// {@template default_auth_error_state_handler}
 /// Base class for handling [DerivAuthErrorState]s. Client app can extend this
 /// class and override the methods to handle the error states based on their
-/// customization. This handler is to make sure each and every error state is
-/// handled.
+/// customization.
 /// {@endtemplate}
 base class AuthErrorStateHandler {
   /// {@macro default_auth_error_state_handler}
@@ -17,7 +16,35 @@ base class AuthErrorStateHandler {
   /// The [BuildContext] of the widget that is using this handler.
   final BuildContext context;
 
-  void _showDialog(DerivAuthErrorState state) {
+  /// On invalid 2FA code.
+  void invalid2faCode(DerivAuthErrorState state) {
+    showErrorDialog(
+      context: context,
+      errorMessage: context.derivAuthLocalization.informInvalid2FACode,
+      actionLabel: context.derivAuthLocalization.actionTryAgain,
+    );
+  }
+
+  /// On account is not activated.
+  void onAccountUnavailable(DerivAuthErrorState state) {
+    showErrorDialog(
+      context: context,
+      errorMessage: context.derivAuthLocalization.informDeactivatedAccount,
+      actionLabel: context.derivAuthLocalization.actionTryAgain,
+    );
+  }
+
+  /// On expired account.
+  void onExpiredAccount(DerivAuthErrorState state) {
+    showErrorDialog(
+      context: context,
+      errorMessage: context.derivAuthLocalization.informExpiredAccount,
+      actionLabel: context.derivAuthLocalization.actionTryAgain,
+    );
+  }
+
+  /// On failed authorization.
+  void onFailedAuthorization(DerivAuthErrorState state) {
     showErrorDialog(
       context: context,
       errorMessage: state.message,
@@ -25,63 +52,75 @@ base class AuthErrorStateHandler {
     );
   }
 
-  /// On invalid 2FA code.
-  void invalid2faCode(DerivAuthErrorState state) {
-    _showDialog(state);
-  }
-
-  /// On account is not activated.
-  void onAccountUnavailable(DerivAuthErrorState state) {
-    _showDialog(state);
-  }
-
-  /// On expired account.
-  void onExpiredAccount(DerivAuthErrorState state) {
-    _showDialog(state);
-  }
-
-  /// On failed authorization.
-  void onFailedAuthorization(DerivAuthErrorState state) {
-    _showDialog(state);
-  }
-
   /// User is trying to authenticate from an unsupported residence.
   void onInavlidResidence(DerivAuthErrorState state) {
-    _showDialog(state);
+    showErrorDialog(
+      context: context,
+      errorMessage: context.derivAuthLocalization.informInvalidResidence,
+      actionLabel: context.derivAuthLocalization.actionTryAgain,
+    );
   }
 
   /// On invalid credentials.
   void onInvalidCredentials(DerivAuthErrorState state) {
-    _showDialog(state);
+    showErrorDialog(
+      context: context,
+      errorMessage: context.derivAuthLocalization.informInvalidCredentials,
+      actionLabel: context.derivAuthLocalization.actionTryAgain,
+    );
   }
 
   /// User has set up 2FA and needs to enter 2FA.
   void onMissingOtp(DerivAuthErrorState state) {
-    _showDialog(state);
+    showErrorDialog(
+      context: context,
+      errorMessage: context.derivAuthLocalization.informMissingOtp,
+      actionLabel: context.derivAuthLocalization.actionTryAgain,
+    );
   }
 
   /// On self closed account.
   void onSelfClosed(DerivAuthErrorState state) {
-    _showDialog(state);
+    showErrorDialog(
+      context: context,
+      errorMessage: context.derivAuthLocalization.informSelfClosed,
+      actionLabel: context.derivAuthLocalization.actionTryAgain,
+    );
   }
 
   /// On unexpected error.
   void onUnexpectedError(DerivAuthErrorState state) {
-    _showDialog(state);
+    showErrorDialog(
+      context: context,
+      errorMessage: context.derivAuthLocalization.informUnexpectedError,
+      actionLabel: context.derivAuthLocalization.actionTryAgain,
+    );
   }
 
   /// Account is not supported in the country.
   void onUnsupportedCountry(DerivAuthErrorState state) {
-    _showDialog(state);
+    showErrorDialog(
+      context: context,
+      errorMessage: context.derivAuthLocalization.informUnsupportedCountry,
+      actionLabel: context.derivAuthLocalization.actionTryAgain,
+    );
   }
 
   /// On connection error.
   void onConnectionError(DerivAuthErrorState state) {
-    _showDialog(state);
+    showErrorDialog(
+      context: context,
+      errorMessage: context.derivAuthLocalization.informConnectionError,
+      actionLabel: context.derivAuthLocalization.actionTryAgain,
+    );
   }
 
   /// On switch account error.
   void onSwitchAccountError(DerivAuthErrorState state) {
-    _showDialog(state);
+    showErrorDialog(
+      context: context,
+      errorMessage: context.derivAuthLocalization.informSwitchAccountError,
+      actionLabel: context.derivAuthLocalization.actionTryAgain,
+    );
   }
 }
