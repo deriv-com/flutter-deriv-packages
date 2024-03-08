be60537e7227b2a4e7b79319f6e3156fd559d18e
sahani-deriv <sahani@deriv.com>
2024-03-08 12:39:29 +0800

    chore: change sdk version

--- packages/deriv_language_selector/lib/src/helpers/language_bottom_sheet_helper.dart
+++ packages/deriv_language_selector/lib/src/helpers/language_bottom_sheet_helper.dart
@@ -1,6 +1,7 @@
 import 'package:deriv_language_selector/deriv_language_selector.dart';
 import 'package:deriv_ui/deriv_ui.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 /// A helper function to show the language bottom sheet.
 void showExpandableLanguageBottomSheet({
@@ -11,9 +12,13 @@ void showExpandableLanguageBottomSheet({
       context: context,
       isScrollControlled: true,
       useRootNavigator: true,
-      builder: (BuildContext context) => ExpandableBottomSheet(
-        title: bottomsheetTitle,
-        upperContent: const LanguageSelector.bottomSheet(),
-        labelContractDetails: '',
+      builder: (BuildContext innerContext) => BlocProvider<LanguageCubit>.value(
+        value: BlocProvider.of<LanguageCubit>(context),
+        child: ExpandableBottomSheet(
+          title: bottomsheetTitle,
+          upperContent: const LanguageSelector.bottomSheet(),
+          // TODO(sahani): Remove labelContractDetails
+          labelContractDetails: '',
+        ),
       ),
     );
