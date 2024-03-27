b7a860a1a945e14c95e18f258175f2da76e6b828
bassam-deriv <bassam.elobeid@regentmarkets.com>
2024-03-27 16:22:36 +0400

    Update localizations

---
+++ packages/deriv_localizations/lib/l10n/generated/deriv_passkeys/deriv_passkeys_localizations_en.dart
@@ -0,0 +1,12 @@
+import 'deriv_passkeys_localizations.dart';
+
+/// The translations for English (`en`).
+class DerivPasskeysLocalizationsEn extends DerivPasskeysLocalizations {
+  DerivPasskeysLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get passkey_created_success_title => 'Success!';
+
+  @override
+  String get passkey_created_success_message => 'Your account is now secured with a passkey. Manage your passkey through your \$platformName account settings.';
+}
