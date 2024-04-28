f76372106e17fe15f2cc56b9f555c7bf2fa7a311
Sagar Deriv <sagar@deriv.com>
2024-04-28 21:39:24 +0400

    fix(deriv_ui): allow only 2 decimal digits for stable crypto currencies

--- packages/deriv_ui/lib/components/numpad/models/currency_detail.dart
+++ packages/deriv_ui/lib/components/numpad/models/currency_detail.dart
@@ -12,7 +12,20 @@ class CurrencyDetail {
   CurrencyDetail(this.amount, this.currencyType);
 
   /// Available fiat currency that Deriv supports.
-  static List<String> fiatCurrencies = <String>['USD', 'AUD', 'EUR', 'GBP'];
+  static List<String> fiatCurrencies = <String>[
+    'USD',
+    'AUD',
+    'EUR',
+    'GBP',
+  ];
+
+  /// Available stable coin currency that Deriv supports.
+  static List<String> stableCurrencies = <String>[
+    'USDC',
+    'USDT',
+    'TUSDT',
+    'EUSDT'
+  ];
 
   /// Amount passed for exchance.
   final double amount;
@@ -23,18 +36,24 @@ class CurrencyDetail {
   /// This will check if this instance is a fiat type currency or not.
   bool get isFiat => fiatCurrencies.contains(currencyType);
 
+  /// This will check if the currency is stable coin currency(USDC,tUSDT,eUSDT) or not.
+  bool get isStableCurrency =>
+      stableCurrencies.contains(currencyType.toUpperCase());
+
   /// Amount that is used to display for user.
   String get displayAmount {
     if (amount == 0.0) {
       return '';
     }
 
-    return isFiat ? amount.toStringAsFixed(2) : amount.toStringAsFixed(8);
+    return isFiat || isStableCurrency
+        ? amount.toStringAsFixed(2)
+        : amount.toStringAsFixed(8);
   }
 
   /// This will give a specific currency formatter based on what type of currency it is like fiat or crypto currency.
   NumberFormat get formatter {
-    if (isFiat) {
+    if (isFiat || isStableCurrency) {
       return NumberFormat.decimalPattern()..maximumFractionDigits = 2;
     } else {
       return NumberFormat.decimalPattern()..maximumFractionDigits = 8;
