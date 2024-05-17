505626776668214f710f30c0b80b8346d1cb0147
bassam-deriv <bassam.elobeid@regentmarkets.com>
2024-05-17 11:23:10 +0400

    refactor(deriv_passkeys): added showErrorDialogue logic.

--- packages/deriv_passkeys/lib/src/presentation/utils/handle_errors_utils.dart
+++ packages/deriv_passkeys/lib/src/presentation/utils/handle_errors_utils.dart
@@ -20,33 +20,39 @@ void handlePasskeysError(BuildContext context, DerivPasskeysErrorState state) {
       ),
     );
   } else {
+    bool showErrorDialogue = false;
     String title = context.derivPasskeysLocalizations.unexpectedError;
     String content = state.message;
 
     if (state.errorCode == 'PASSKEYS_SERVICE_ERROR') {
       content = state.message;
+      showErrorDialogue = true;
     }
     if (state.errorCode == 'PASSKEYS_NOT_FOUND') {
       title = context.derivPasskeysLocalizations.noPasskeyFound;
       content = state.message;
+      showErrorDialogue = true;
     }
 
     if (state.errorCode == 'PASSKEYS_OFF' || state.errorCode == 'PasskeysOff') {
       title = context.derivPasskeysLocalizations.unable_to_process_your_request;
       content = context.derivPasskeysLocalizations
           .unable_to_process_your_request_description;
+      showErrorDialogue = true;
     }
 
-    showDialog<String>(
-      context: context,
-      builder: (BuildContext context) => PopupAlertDialog(
-        title: title,
-        content: Text(content),
-        positiveButtonLabel: context.derivPasskeysLocalizations.ok,
-        onPositiveActionPressed: () {
-          Navigator.pop(context, context.derivPasskeysLocalizations.ok);
-        },
-      ),
-    );
+    if (showErrorDialogue) {
+      showDialog<String>(
+        context: context,
+        builder: (BuildContext context) => PopupAlertDialog(
+          title: title,
+          content: Text(content),
+          positiveButtonLabel: context.derivPasskeysLocalizations.ok,
+          onPositiveActionPressed: () {
+            Navigator.pop(context, context.derivPasskeysLocalizations.ok);
+          },
+        ),
+      );
+    }
   }
 }
