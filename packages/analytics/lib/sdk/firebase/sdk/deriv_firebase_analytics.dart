9081c8caf62c0a4b2d1d389737fec00cf5565c3a
abedelaziz-deriv <abed@regentmarkets.com>
2024-06-20 10:08:43 +0800

    Updated firebase dependencies

--- packages/analytics/lib/sdk/firebase/sdk/deriv_firebase_analytics.dart
+++ packages/analytics/lib/sdk/firebase/sdk/deriv_firebase_analytics.dart
@@ -69,7 +69,7 @@ class DerivFirebaseAnalytics implements BaseAnalytics<FirebaseConfiguration> {
   /// Logs a custom Flutter Analytics event with the given [name] and event [parameters].
   Future<bool> logEvent({
     required String name,
-    Map<String, dynamic>? parameters,
+    Map<String, Object>? parameters,
   }) async =>
       _execute(() async {
         await _firebaseAnalytics.logEvent(
