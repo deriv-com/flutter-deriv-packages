6b1b99bd3f0cbfbf1884321321cda95d1c1952e1
ramin-deriv <ramin.vakili@deriv.com>
2024-06-21 11:16:12 +0800

    add localizations context extension

--- packages/deriv_mobile_chart_wrapper/lib/deriv_mobile_chart_wrapper.dart
+++ packages/deriv_mobile_chart_wrapper/lib/deriv_mobile_chart_wrapper.dart
@@ -1,7 +1,1 @@
 library deriv_mobile_chart_wrapper;
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
---
+++ packages/deriv_mobile_chart_wrapper/lib/src/extensions.dart
@@ -0,0 +1,10 @@
+import 'package:deriv_localizations/l10n/generated/deriv_mobile_chart_wrapper/deriv_mobile_chart_wrapper_localizations.dart';
+import 'package:flutter/material.dart';
+
+/// Extension for [BuildContext]
+extension ContextExtension on BuildContext {
+  /// Get derivPasskeysLocalizations.
+  DerivMobileChartWrapperLocalizations
+      get derivMobileChartWrapperLocalizations =>
+          DerivMobileChartWrapperLocalizations.of(this);
+}
