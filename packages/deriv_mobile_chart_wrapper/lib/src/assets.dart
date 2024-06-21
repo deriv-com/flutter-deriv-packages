10d488a548de6cc7f169bfda08e79c24fc198fd5
ramin-deriv <ramin.vakili@deriv.com>
2024-06-21 14:05:40 +0800

    add indicator model and update assets

---
+++ packages/deriv_mobile_chart_wrapper/lib/src/assets.dart
@@ -0,0 +1,6 @@
+const String iconAssetsFolder = 'assets/icons/';
+
+const String macdIcon = '${iconAssetsFolder}ic_macd.svg';
+const String rsiIcon = '${iconAssetsFolder}ic_rsi.svg';
+const String bollingerBandsIcon = '${iconAssetsFolder}ic_bollinger_bands.svg';
+const String movingAverageIcon = '${iconAssetsFolder}ic_moving_average.svg';
--- packages/deriv_mobile_chart_wrapper/lib/src/mobile_tools_ui/mobile_tools_bottom_sheet_content.dart
+++ packages/deriv_mobile_chart_wrapper/lib/src/mobile_tools_ui/mobile_tools_bottom_sheet_content.dart
@@ -1,4 +1,6 @@
+import 'package:deriv_mobile_chart_wrapper/src/assets.dart';
 import 'package:deriv_mobile_chart_wrapper/src/mobile_tools_ui/chips_list.dart';
+import 'package:deriv_mobile_chart_wrapper/src/models/indicator_item_model.dart';
 import 'package:deriv_theme/deriv_theme.dart';
 import 'package:deriv_mobile_chart_wrapper/src/extensions.dart';
 import 'package:flutter/material.dart';
@@ -11,12 +13,23 @@ class MobileToolsBottomSheetContent extends StatelessWidget {
   /// Initializes the bottom sheet content.
   const MobileToolsBottomSheetContent({super.key});
 
+  static const List<IndicatorItemModel> indicators = [
+    IndicatorItemModel(title: 'MACD', icon: macdIcon),
+    IndicatorItemModel(title: 'Relative Strength Index (RSI)', icon: rsiIcon),
+    IndicatorItemModel(title: 'Moving Average', icon: movingAverageIcon),
+    IndicatorItemModel(title: 'Bollinger Bands', icon: bollingerBandsIcon),
+  ];
+
   @override
   Widget build(BuildContext context) => Column(
         children: <Widget>[
           _buildHeader(context),
           ChipsList(items: <CustomChip>[
-            CustomChip(),
+            CustomChip(title: 'Active'),
+            CustomChip(title: 'All'),
+            CustomChip(title: 'Momentum'),
+            CustomChip(title: 'Volatility'),
+            CustomChip(title: 'Moving average'),
           ]),
         ],
       );
