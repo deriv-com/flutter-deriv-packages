8aafe052e16e394ac90a1a0aa68186da95d58a07
Osama <osama@deriv.com>
2024-07-30 17:15:04 +0800

    add documentation

--- packages/deriv_mobile_chart_wrapper/lib/src/pages/base_setting_page.dart
+++ packages/deriv_mobile_chart_wrapper/lib/src/pages/base_setting_page.dart
@@ -1,10 +1,15 @@
 import 'package:deriv_chart/deriv_chart.dart';
 
+/// The base class for the indicator setting page.
 abstract class BaseIndicatorSettingPage<T extends IndicatorConfig> {
+  /// Initializes the indicator setting page.
   BaseIndicatorSettingPage(
       {required this.initialConfig, required this.onConfigUpdated});
 
+  /// The initial configuration of the indicator.
   T initialConfig;
 
+
+  /// The callback function to be called when the configuration is updated.
   void Function(T config) onConfigUpdated;
 }
