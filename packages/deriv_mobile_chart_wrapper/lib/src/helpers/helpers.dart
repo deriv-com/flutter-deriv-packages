64ae6534e7e7b257be356c5926b25e88061517fb
sahani-deriv <125638269+sahani-deriv@users.noreply.github.com>
2024-08-02 16:58:06 +0800

    Update helpers.dart

--- packages/deriv_mobile_chart_wrapper/lib/src/helpers/helpers.dart
+++ packages/deriv_mobile_chart_wrapper/lib/src/helpers/helpers.dart
@@ -2,21 +2,6 @@ import 'package:deriv_mobile_chart_wrapper/deriv_mobile_chart_wrapper.dart';
 import 'package:deriv_mobile_chart_wrapper/src/extensions.dart';
 import 'package:flutter/material.dart';
 
-const List<Color> availableColors = [
-  Color(0xFFFFFFFF), // White
-  Color(0xFFF39230), // Orange
-  Color(0xFFEF6B53), // Deep Orange
-  Color(0xFFD73737), // Red
-  Color(0xFF03BFF0), // Light Blue
-  Color(0xFF3271B4), // Blue
-  Color(0xFF2FBCB5), // Teal
-  Color(0xFF8EC648), // Light Green
-  Color(0xFF48A25C), // Green
-  Color(0xFFFFF224), // Yellow
-  Color(0xFFEE6EA9), // Pink
-  Color(0xFF853289), // Purple
-];
-
 Map<String, String> getSourcesOptions(BuildContext context) => {
       'close': context.mobileChartWrapperLocalizations.labelClose,
       'open': context.mobileChartWrapperLocalizations.labelOpen,
