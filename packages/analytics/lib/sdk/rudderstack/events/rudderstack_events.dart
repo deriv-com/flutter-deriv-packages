feb0936d80c1f3abff0716e7842ef2f4df1d51f6
Illya Tsai <ilya@regentmarkets.com>
2024-08-09 16:00:34 +0300

    fix formatting

--- packages/analytics/lib/sdk/rudderstack/events/rudderstack_events.dart
+++ packages/analytics/lib/sdk/rudderstack/events/rudderstack_events.dart
@@ -36,7 +36,10 @@ class DerivRudderstackEvents {
   /// Tracks system error has happened,
   /// like no connection to the server and etc.
   void logAccountInfo(
-      String? accountType, String? countryResidence, String? language) {
+    String? accountType,
+    String? countryResidence,
+    String? language,
+  ) {
     DerivRudderstack().track(
       eventName: 'account_info',
       properties: <String, dynamic>{
