b5278ebf2dce4ae0d6e586c82e0dcb4105420f0a
sahani-deriv <sahani@deriv.com>
2024-08-29 17:28:21 +0800

    fix: analysis issue

--- packages/deriv_ui/lib/components/bottom_sheet/deriv_bottom_sheet.dart
+++ packages/deriv_ui/lib/components/bottom_sheet/deriv_bottom_sheet.dart
@@ -90,7 +90,6 @@ class _DerivBottomSheetState extends State<DerivBottomSheet> {
             ),
           ),
         ),
-        onDispose: widget.onDispose,
       );
 
   Widget _buildTopHandle() => Container(
