14737f0c7142549e651739d232f709b701935650
aliakbar-deriv <aliakbar.kondath@regentmarkets.com>
2024-09-05 18:04:32 +0400

    Update localizations

--- packages/deriv_localizations/lib/l10n/generated/deriv_mobile_chart_wrapper/deriv_mobile_chart_wrapper_localizations.dart
+++ packages/deriv_localizations/lib/l10n/generated/deriv_mobile_chart_wrapper/deriv_mobile_chart_wrapper_localizations.dart
@@ -79,8 +79,8 @@ abstract class DerivMobileChartWrapperLocalizations {
 
   final String localeName;
 
-  static DerivMobileChartWrapperLocalizations of(BuildContext context) {
-    return Localizations.of<DerivMobileChartWrapperLocalizations>(context, DerivMobileChartWrapperLocalizations)!;
+  static DerivMobileChartWrapperLocalizations? of(BuildContext context) {
+    return Localizations.of<DerivMobileChartWrapperLocalizations>(context, DerivMobileChartWrapperLocalizations);
   }
 
   static const LocalizationsDelegate<DerivMobileChartWrapperLocalizations> delegate = _DerivMobileChartWrapperLocalizationsDelegate();
@@ -632,6 +632,42 @@ abstract class DerivMobileChartWrapperLocalizations {
   /// In en, this message translates to:
   /// **'Range {min} - {max}'**
   String warnRangeMinMax(Object max, Object min);
+
+  /// No description provided for @labelDrawingTools.
+  ///
+  /// In en, this message translates to:
+  /// **'Drawing tools'**
+  String get labelDrawingTools;
+
+  /// No description provided for @labelTools.
+  ///
+  /// In en, this message translates to:
+  /// **'Tools'**
+  String get labelTools;
+
+  /// No description provided for @labelLine.
+  ///
+  /// In en, this message translates to:
+  /// **'Line'**
+  String get labelLine;
+
+  /// No description provided for @labelRay.
+  ///
+  /// In en, this message translates to:
+  /// **'Ray'**
+  String get labelRay;
+
+  /// No description provided for @informTapToSetFirstPoint.
+  ///
+  /// In en, this message translates to:
+  /// **'Tap to set first point'**
+  String get informTapToSetFirstPoint;
+
+  /// No description provided for @informTapToSetFinalPoint.
+  ///
+  /// In en, this message translates to:
+  /// **'Tap to set final point'**
+  String get informTapToSetFinalPoint;
 }
 
 class _DerivMobileChartWrapperLocalizationsDelegate extends LocalizationsDelegate<DerivMobileChartWrapperLocalizations> {
