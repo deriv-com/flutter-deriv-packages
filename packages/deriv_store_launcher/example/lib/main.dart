8f52e6f8809e8899d98916219c0b5ad3cc58c9f2
aliakbar-deriv <aliakbar.kondath@regentmarkets.com>
2024-10-21 19:39:11 +0400

    feat!: [Breaking change] added new functionalities and refactored the existing. Now supports app launch and store launch for both iOS and Android platform.

--- packages/deriv_store_launcher/example/lib/main.dart
+++ packages/deriv_store_launcher/example/lib/main.dart
@@ -1,43 +1,159 @@
-import 'dart:io';
-
+import 'package:deriv_store_launcher/deriv_store_launcher.dart';
+import 'package:deriv_store_launcher/platform_checker.dart';
 import 'package:flutter/material.dart';
 
-import 'package:deriv_store_launcher/deriv_store_launcher.dart';
-
-void main() => runApp(const App());
-
-/// This Widget is the main application widget.
-class App extends StatefulWidget {
-  /// initializes [App].
-  const App({Key? key}) : super(key: key);
-
-  @override
-  State<App> createState() => _AppState();
+void main() {
+  runApp(MyApp());
 }
 
-class _AppState extends State<App> {
-  final bool isAndroid = Platform.isAndroid;
-
+/// The main app widget.
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) => MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            elevation: 0,
-            title: const Text('Store Luncher Example App'),
+        title: 'App Launcher Example App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: MyHomePage(),
+      );
+}
+
+/// The home page widget.
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: const Text('App Launcher Example App'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ElevatedButton(
+                onPressed: () => _openApp(
+                  context,
+                  packageName: 'com.deriv.dp2p',
+                  urlScheme: 'deriv://',
+                  appStoreId: '1506901451',
+                  openStore: true,
+                ),
+                child: const Text('Open DP2P'),
+              ),
+              ElevatedButton(
+                onPressed: () => _openApp(
+                  context,
+                  packageName: 'com.deriv.dx',
+                  urlScheme: 'derivx://',
+                  appStoreId: '1563337503',
+                  openStore: true,
+                ),
+                child: const Text('Open DerivX'),
+              ),
+              ElevatedButton(
+                onPressed: () => _checkAppInstalled(
+                  context,
+                  packageName: 'com.deriv.dp2p',
+                  urlScheme: 'deriv://',
+                ),
+                child: const Text('Check DP2P Installed'),
+              ),
+              ElevatedButton(
+                onPressed: () => _checkAppInstalled(
+                  context,
+                  packageName: 'com.deriv.dx',
+                  urlScheme: 'derivx://',
+                ),
+                child: const Text('Check DerivX Installed'),
+              ),
+              ElevatedButton(
+                onPressed: () => _openStoreListing(
+                  packageName: 'com.deriv.dp2p',
+                  urlScheme: 'deriv://',
+                  appStoreId: '1506901451',
+                ),
+                child: const Text('Open DP2P in Store'),
+              ),
+              ElevatedButton(
+                onPressed: () => _openStoreListing(
+                  packageName: 'com.deriv.dx',
+                  urlScheme: 'derivx://',
+                  appStoreId: '1563337503',
+                ),
+                child: const Text('Open DerivX in Store'),
+              ),
+            ],
           ),
-          body: Center(
-            child: TextButton(
-              style: TextButton.styleFrom(
-                foregroundColor: Colors.white,
-                backgroundColor: isAndroid ? Colors.blue : Colors.green,
-              ),
-              child:
-                  Text('${isAndroid ? 'Open Play Store' : 'Open App Store'}'),
-              onPressed: () => DerivStoreLauncher.openWithStore(
-                isAndroid ? 'com.deriv.dp2p' : '1506901451',
-              ),
-            ),
+        ),
+        bottomNavigationBar: Container(
+          padding: const EdgeInsets.all(16),
+          color: Colors.grey[200],
+          child: Text(
+            'Platform detected: [${PlatformChecker().isIOS ? 'iOS' : 'Android'}'
+            ']',
           ),
         ),
       );
+
+  void _showSnackbar(BuildContext context, {required String message}) {
+    final SnackBar snackBar = SnackBar(content: Text(message));
+
+    // Show the Snackbar
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  Future<void> _openApp(
+    BuildContext context, {
+    required String packageName,
+    required String urlScheme,
+    required String appStoreId,
+    required bool openStore,
+  }) async {
+    final bool isAppOpened = await DerivStoreLauncher.openApp(
+      androidPackageName: packageName,
+      iosUrlScheme: urlScheme,
+      iosAppStoreId: appStoreId,
+      openStore: openStore,
+    );
+
+    if (!isAppOpened) {
+      String message = '';
+      if (!openStore) {
+        message = 'The app is not installed and configured to don\'t open the '
+            'store.';
+      } else {
+        message = 'The app is not installed.';
+      }
+
+      _showSnackbar(context, message: message);
+    }
+  }
+
+  Future<void> _checkAppInstalled(
+    BuildContext context, {
+    required String packageName,
+    required String urlScheme,
+  }) async {
+    final bool? isAppInstalled = await DerivStoreLauncher.isAppInstalled(
+      androidPackageName: packageName,
+      iosUrlScheme: urlScheme,
+    );
+
+    if (isAppInstalled != null) {
+      _showSnackbar(context,
+          message: '${urlScheme.split("://")[0]} is '
+              '${isAppInstalled ? 'installed' : 'not installed'}');
+    }
+  }
+
+  Future<void> _openStoreListing({
+    required String packageName,
+    required String urlScheme,
+    required String appStoreId,
+  }) async {
+    await DerivStoreLauncher.openStoreListing(
+      androidPackageName: packageName,
+      iosUrlScheme: urlScheme,
+      iosAppStoreId: appStoreId,
+    );
+  }
 }
